59f819fda425949ffcb613d7a5913bec74a40f23
Ramiro Magno <rmagno@pattern.institute>
2024-04-28 02:54:57 +0100

    Whitelist words

---
+++ tools/dic.R
@@ -0,0 +1,16 @@
+words <-
+  c(
+    "IgG",
+    "NADH",
+    "Promega",
+    "Sisti",
+    "et",
+    "al",
+    "bp",
+    "pGEM",
+    "plasmid",
+    "qPCR",
+    "quercetin",
+    "tannic"
+  )
+saveRDS(words, file = ".aspell/custom_dic.rds", version = 2)
